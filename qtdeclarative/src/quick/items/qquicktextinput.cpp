630da5d4e906c721f22162ac0178bb4dc2e3f9bd
CipSoft-Tibia <56713293+CipSoft-Tibia@users.noreply.github.com>
2019-10-18 09:29:02 +0200

    Respect text antialiasing property for TextEdit and TextInput

--- qtdeclarative/src/quick/items/qquicktextinput.cpp
+++ qtdeclarative/src/quick/items/qquicktextinput.cpp
@@ -440,6 +440,10 @@ void QQuickTextInput::setFont(const QFont &font)
     d->sourceFont = font;
     QFont oldFont = d->font;
     d->font = font;
+
+    if (!antialiasing())
+        d->font.setStyleStrategy(QFont::NoAntialias);
+
     if (d->font.pointSizeF() != -1) {
         // 0.5pt resolution
         qreal size = qRound(d->font.pointSizeF()*2.0);
@@ -1844,6 +1848,16 @@ void QQuickTextInput::itemChange(ItemChange change, const ItemChangeData &value)
             d->updateLayout();
         }
         break;
+
+    case ItemAntialiasingHasChanged: {
+        if (!antialiasing())
+            d->font.setStyleStrategy(QFont::NoAntialias);
+        else
+            d->font.setStyleStrategy(QFont::PreferAntialias);
+        d->updateLayout();
+        updateCursorRectangle();
+        break;
+    }
 
     default:
         break;
