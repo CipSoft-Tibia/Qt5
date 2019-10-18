9735f7d821e6ff6e7110b8e6b88c34142ca3d31a
CipSoft-Tibia <56713293+CipSoft-Tibia@users.noreply.github.com>
2019-10-18 09:30:47 +0200

    Add glyph cache to software renderer to speed up text rendering

--- qtdeclarative/src/quick/scenegraph/adaptations/software/qsgsoftwareglyphnode.cpp
+++ qtdeclarative/src/quick/scenegraph/adaptations/software/qsgsoftwareglyphnode.cpp
@@ -44,6 +44,7 @@ QT_BEGIN_NAMESPACE
 QSGSoftwareGlyphNode::QSGSoftwareGlyphNode()
     : m_geometry(QSGGeometry::defaultAttributes_TexturedPoint2D(), 0)
     , m_style(QQuickText::Normal)
+    , m_Dirty(true)
 {
     setMaterial((QSGMaterial*)1);
     setGeometry(&m_geometry);
@@ -83,21 +84,25 @@ void QSGSoftwareGlyphNode::setGlyphs(const QPointF &position, const QGlyphRun &g
     m_position = position;
     m_glyphRun = glyphs;
     m_bounding_rect = calculateBoundingRect(position, glyphs);
+    m_Dirty = true;
 }
 
 void QSGSoftwareGlyphNode::setColor(const QColor &color)
 {
     m_color = color;
+    m_Dirty = true;
 }
 
 void QSGSoftwareGlyphNode::setStyle(QQuickText::TextStyle style)
 {
     m_style = style;
+    m_Dirty = true;
 }
 
 void QSGSoftwareGlyphNode::setStyleColor(const QColor &color)
 {
     m_styleColor = color;
+    m_Dirty = true;
 }
 
 QPointF QSGSoftwareGlyphNode::baseLine() const
@@ -115,34 +120,52 @@ void QSGSoftwareGlyphNode::update()
 
 void QSGSoftwareGlyphNode::paint(QPainter *painter)
 {
-    painter->setBrush(QBrush());
-    QPointF pos = m_position - QPointF(0, m_glyphRun.rawFont().ascent());
+    constexpr QPointF STARTING_POINT_IN_PIXMAP{1, 1}; // Leave a 1px top/left spacing for outline
+    if (m_Dirty) {
+        constexpr int OFFSET_FOR_OUTLINE_PIXELS{2}; // Needs 1px on top/left and 1px on bottom/right for outline
+        m_CachedPixmap = QPixmap{
+            static_cast<int>(m_bounding_rect.x() + m_bounding_rect.width() + OFFSET_FOR_OUTLINE_PIXELS),
+            static_cast<int>(m_bounding_rect.y() + m_bounding_rect.height() + OFFSET_FOR_OUTLINE_PIXELS)};
+        const QColor Transparent{0, 0, 0, 0};
+        m_CachedPixmap.fill(Transparent);
 
-    qreal offset = 1.0;
-    if (painter->device()->devicePixelRatioF() > 0.0)
-        offset = 1.0 / painter->device()->devicePixelRatioF();
+        QPainter Painter(&m_CachedPixmap);
+        Painter.setBrush(QBrush());
+        Painter.setBackgroundMode(Qt::TransparentMode);
 
-    switch (m_style) {
-    case QQuickText::Normal: break;
-    case QQuickText::Outline:
-        painter->setPen(m_styleColor);
-        painter->drawGlyphRun(pos + QPointF(0, offset), m_glyphRun);
-        painter->drawGlyphRun(pos + QPointF(0, -offset), m_glyphRun);
-        painter->drawGlyphRun(pos + QPointF(offset, 0), m_glyphRun);
-        painter->drawGlyphRun(pos + QPointF(-offset, 0), m_glyphRun);
-        break;
-    case QQuickText::Raised:
-        painter->setPen(m_styleColor);
-        painter->drawGlyphRun(pos + QPointF(0, offset), m_glyphRun);
-        break;
-    case QQuickText::Sunken:
-        painter->setPen(m_styleColor);
-        painter->drawGlyphRun(pos + QPointF(0, -offset), m_glyphRun);
-        break;
+        qreal offset = 1.0;
+        if (painter->device()->devicePixelRatioF() > 0.0)
+            offset = 1.0 / painter->device()->devicePixelRatioF();
+
+        switch (m_style) {
+        case QQuickText::Normal: break;
+        case QQuickText::Outline:
+            Painter.setPen(m_styleColor);
+            Painter.drawGlyphRun(STARTING_POINT_IN_PIXMAP + QPointF(0, offset), m_glyphRun);
+            Painter.drawGlyphRun(STARTING_POINT_IN_PIXMAP + QPointF(0, -offset), m_glyphRun);
+            Painter.drawGlyphRun(STARTING_POINT_IN_PIXMAP + QPointF(offset, 0), m_glyphRun);
+            Painter.drawGlyphRun(STARTING_POINT_IN_PIXMAP + QPointF(-offset, 0), m_glyphRun);
+            break;
+
+        // XXX: raised and sunken text was never tested, so it might not work as expected
+        case QQuickText::Raised:
+            Painter.setPen(m_styleColor);
+            Painter.drawGlyphRun(STARTING_POINT_IN_PIXMAP + QPointF(0, offset), m_glyphRun);
+            break;
+        case QQuickText::Sunken:
+            Painter.setPen(m_styleColor);
+            Painter.drawGlyphRun(STARTING_POINT_IN_PIXMAP + QPointF(0, -offset), m_glyphRun);
+            break;
+        }
+
+        Painter.setPen(m_color);
+        Painter.drawGlyphRun(STARTING_POINT_IN_PIXMAP, m_glyphRun);
+
+        m_Dirty = false;
     }
 
-    painter->setPen(m_color);
-    painter->drawGlyphRun(pos, m_glyphRun);
+    QPointF AbsolutePos = m_position - QPointF(0, m_glyphRun.rawFont().ascent()) - STARTING_POINT_IN_PIXMAP;
+    painter->drawPixmap(AbsolutePos, m_CachedPixmap);
 }
 
 QT_END_NAMESPACE
