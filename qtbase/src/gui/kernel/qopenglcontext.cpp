fd1e756107bc3a96d42c6869bbc67352f7d5dd89
CipSoft-Tibia <56713293+CipSoft-Tibia@users.noreply.github.com>
2022-05-19 11:34:22 +0200

    Revert forced text rendering workaround on M1
    This change introduced by https://bugreports.qt.io/browse/QTBUG-89379
    is probably responsible for corrupting text rendering on Apple M1 cpus
    when using Text.NativeRendering.

--- qtbase/src/gui/kernel/qopenglcontext.cpp
+++ qtbase/src/gui/kernel/qopenglcontext.cpp
@@ -1023,8 +1023,7 @@ bool QOpenGLContext::makeCurrent(QSurface *surface)
                         || qstrcmp(rendererString, "GC800 core") == 0
                         || qstrcmp(rendererString, "GC1000 core") == 0
                         || strstr(rendererString, "GC2000") != 0
-                        || qstrcmp(rendererString, "Immersion.16") == 0
-                        || qstrncmp(rendererString, "Apple Mx", 7) == 0;
+                        || qstrcmp(rendererString, "Immersion.16") == 0;
         }
         needsWorkaroundSet = true;
     }
